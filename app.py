69a1d1c640b341128289d0910585e37875370b48
Mekan777-alt <mekan2678@gmail.com>
2023-05-27 16:31:17 +0300

    add database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from config import dp
+from config import dp, db
 import logging
 from aiogram import executor
 import hendlers
@@ -6,6 +6,7 @@ import hendlers
 
 async def on_startup(dp):
     logging.basicConfig(level=logging.INFO)
+    db.create_tables()
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
---
+++ data/data.py
@@ -0,0 +1,39 @@
+import sqlite3 as sq
+
+
+class Database(object):
+
+    def __init__(self, path):
+        self.connection = sq.connect(path)
+        self.connection.execute('pragma foreign_keys = on')
+        self.connection.commit()
+        self.cur = self.connection.cursor()
+
+    def create_tables(self):
+        self.query(
+            'CREATE TABLE IF NOT EXISTS users (id int, visa text, ariza_shakli text, davlat text,'
+            ' shahar text, visametric text, turlari text, soni int)')
+
+    def query(self, arg, values=None):
+        if values is None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        self.connection.commit()
+
+    def fetchone(self, arg, values=None):
+        if values is None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        return self.cur.fetchone()
+
+    def fetchall(self, arg, values=None):
+        if values is None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        return self.cur.fetchall()
+
+    def __del__(self):
+        self.connection.close()
